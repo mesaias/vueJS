b863aafcd30b90ab906cbca71830b05bf4bf8eec
Isaias <jisolorioa@concredito.com.mx>
2017-06-29 19:26:58 -0600

    primer commit

---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> The Home component (te la shupo) </h1>   
+</template>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,25 @@
+<template>
+<div>   
+    <app-stock v-for="sotck in stocks"></app-stock>
+    </div>
+</template>
+
+<script>
+    import Stock from './Stock.vue';
+
+export default {
+    data(){
+        return{
+            stocks:[
+                { id:1, name: 'CARLITOS', edad: 24 },
+                { id:2, name: 'YORSH', edad: 25 },
+                { id:3, name: 'ALAN', edad: 20 },
+                { id:4, name: 'ISAIAS', edad: 23 }
+            ]
+        }
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
